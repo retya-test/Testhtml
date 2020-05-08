6d70ac714409a6f331f54d54d509f7eee87708a0
root <sip.retyamahendra.com>
2020-05-08 06:37:44 +0000

    7th Test CI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('This is CI bro :)');
+  res.send('Test work :)');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
